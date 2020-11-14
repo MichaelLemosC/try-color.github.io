a4cd0757f39dd1afbbd8ba891ddae9684d041bcc
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-11-14 18:09:31 -0300

    Adiciona index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,43 @@
+const bolas = document.getElementsByClassName('ball');
+const textoCor = document.getElementById('rgb-color');
+const textoResposta = document.getElementById('answer');
+const botaoReiniciar = document.getElementById('reset-game');
+const cores = [];
+
+function numeroAleatorio(mul) {
+  const numero = Math.floor(Math.random() * mul);
+  return numero;
+}
+
+function corAleatoria() {
+  const rgb = `rgb(${numeroAleatorio(256)}, ${numeroAleatorio(256)}, ${numeroAleatorio(256)})`;
+  return rgb;
+}
+
+function cliqueNaBola(event) {
+  const corSelecionada = event.target.style.backgroundColor;
+  if (corSelecionada === textoCor.textContent) {
+    textoResposta.textContent = 'Acertou!';
+  } else {
+    textoResposta.textContent = 'Errou! Tente novamente!';
+  }
+}
+
+for (let bola = 0; bola < bolas.length; bola += 1) {
+  const cor = corAleatoria();
+  bolas[bola].style.backgroundColor = cor;
+  cores[bola] = cor;
+  bolas[bola].addEventListener('click', cliqueNaBola);
+}
+
+function adicionaCorAleatoria() {
+  textoCor.textContent = cores[numeroAleatorio(6)];
+}
+
+adicionaCorAleatoria();
+
+function reiniciaJogo() {
+  window.location.reload();
+}
+
+botaoReiniciar.addEventListener('click', reiniciaJogo);
